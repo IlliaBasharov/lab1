1c7920b93010d3a32f08de7e79ce28b522a14263
Illia <illiabasharov@gmail.com>
2019-11-05 21:12:34 +0200

    add comments

--- listajax.php
+++ listajax.php
@@ -3,7 +3,7 @@ header('Content-Type: text/html; charset=utf-8');
 if (!file_exists('rates.ini')) {
     die();
 }
-$data = parse_ini_file('rates.ini', true);
+$data = parse_ini_file('rates.ini', true);//обрабатывает конфигурационный файл и возвращает его настройки ввиде ассоциативного массива
 foreach ($data as $k => $v) {
     echo '<option value="'.$k.'">'.$k.'</option>\n';
 }
